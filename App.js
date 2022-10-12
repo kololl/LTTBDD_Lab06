b3844e4049ffd2c55311027ac6eb7c2c24453dbe
kololl <tieuhivong7201@gmail.com>
2022-10-12 10:00:19 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,153 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+ 
+import { StyleSheet, FlatList, Text, View, Alert, TouchableOpacity, TextInput , Button} from 'react-native';
+ 
+export default class Myproject extends Component {
+ 
+  constructor(props) {
+    
+    super(props);
+ 
+    this.array = [{
+      title: 'Học java'
+    },
+    {
+      title: 'C#'
+    }
+   
+    ],
+ 
+      this.state = {
+ 
+        arrayHolder: [],
+ 
+        textInput_Holder: ''
+ 
+      }
+ 
+  }
+ 
+  componentDidMount() {
+ 
+    this.setState({ arrayHolder: [...this.array] })
+ 
+  }
+ 
+ 
+  joinData = () => {
+ 
+    this.array.push({title : this.state.textInput_Holder});
+ 
+    this.setState({ arrayHolder: [...this.array] })
+ 
+  }
+ 
+  FlatListItemSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          width: "100%",
+          backgroundColor: "#607D8B",
+        }}
+        
+      >
+       
+        </View>
+    );
+  }
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  GetItem(item) {
+ 
+    Alert.alert(item);
+ 
+  }
+ 
+ 
+  render() {
+    return (
+ 
+      <View style={styles.MainContainer}>
+        
+ 
+        <TextInput 
+          placeholder="Nhap item"
+          onChangeText={data => this.setState({ textInput_Holder: data })}
+          style={styles.textInputStyle}
+          underlineColorAndroid='transparent'
+        />
+ 
+        <TouchableOpacity onPress={this.joinData} activeOpacity={0.7} style={styles.button} >
+ 
+          <Text style={styles.buttonText}> Add </Text>
+ 
+        </TouchableOpacity>
+ 
+        <FlatList
+ 
+          data={this.state.arrayHolder}
+ 
+          width='100%'
+ 
+          extraData={this.state.arrayHolder}
+ 
+          keyExtractor={(index) => index.toString()}
+ 
+          ItemSeparatorComponent={this.FlatListItemSeparator}
+ 
+          renderItem={({ item }) => <Text style={styles.item} onPress={this.GetItem.bind(this, item.title)} > {item.title} </Text>}
+        />
+ 
+ 
+      </View>
+ 
+    );
+  }
 }
-
+ 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+ 
+  MainContainer: {
+ 
     justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    margin: 2
+ 
   },
+ 
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+ 
+  textInputStyle: {
+ 
+    textAlign: 'center',
+    height: 40,
+    width: '90%',
+    borderWidth: 1,
+    borderColor: '#4CAF50',
+    borderRadius: 7,
+    marginTop: 30,
+    
+  },
+ 
+  button: {
+ 
+    width: '90%',
+    height: 40,
+    padding: 10,
+    backgroundColor: '#4CAF50',
+    borderRadius: 8,
+    marginTop: 10
+  },
+ 
+  buttonText: {
+    color: '#fff',
+    textAlign: 'center',
+  
+  },
+ 
 });
